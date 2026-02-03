5efaf1d8af96c2aad6d67e28da1cb81b7e9b0eb8
Justin Hoang <justinhoang710@gmail.com>
2026-02-03 11:24:29 -0500

    deleted app.py in order to have a cleaner front end

---
+++ server.py
@@ -0,0 +1,13 @@
+"""Flask entrypoint for the FinGen web dashboard."""
+import os
+
+from webapp import create_app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", port=port, debug=debug)
